72484e1655175b996db55196b9db05de35cbd4eb
Wellington Aguiar <wellington_aguiar@outlook.com>
2024-08-13 15:22:38 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import skinAhri from './skinahri.jpeg';
+
+function Profile() {
+  return (
+    <div style={{ marginBottom: '20px' }}>
+      <img
+        src={skinAhri}
+        alt="Ahri League Of Legends"
+        style={{ width: '450px', height: 'auto' }}
+      />
+      <p>Ahri - A Raposa de Nove Caudas</p> {/* Texto descritivo */}
+    </div>
+  );
+}
+
+function Gallery() {
+  return (
+    <section>
+      <h1>Melhor Campeã do League Of Legends</h1>
+      <Profile />
+      <hr />
+      <Profile />
+      <hr />
+      <Profile />
+    </section>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Gallery />
     </div>
   );
 }
